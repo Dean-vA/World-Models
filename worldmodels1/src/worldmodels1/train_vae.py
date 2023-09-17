0177b8a174134a694cfe3fe83dc4a012363a1a5e
Dean <deanvanaswegen.dva@gmail.com>
2023-09-17 19:34:57 +0200

    typo

--- worldmodels1/src/worldmodels1/train_vae.py
+++ worldmodels1/src/worldmodels1/train_vae.py
@@ -100,7 +100,7 @@ for epoch in range(args.epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        logging.dinfo(f'Completed batch {batch_idx} with loss {loss.item()}')
+        logging.info(f'Completed batch {batch_idx} with loss {loss.item()}')
 
     logging.info(f'Epoch [{epoch + 1}/{args.epochs}], Total Loss: {loss.item()}, Reconstruction Loss: {recon_loss.item()}, KL Divergence: {kl_div.item()}')
 
