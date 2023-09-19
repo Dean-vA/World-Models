2f0fdd0df981495233cdbfaec63b4a54d53fb47b
Dean <deanvanaswegen.dva@gmail.com>
2023-09-19 19:52:56 +0200

    ,

--- worldmodels1/src/worldmodels1/create_latent_data.py
+++ worldmodels1/src/worldmodels1/create_latent_data.py
@@ -87,6 +87,8 @@ def main(args):
                     latent_vectors = z.cpu().numpy()
                     if args.get_metadata:
                         for latent, action, episode, step in zip(latent_vectors, actions, episodes, steps):
+                            print(f'Latent shape: {latent.shape}, action shape: {action.shape}, episode shape: {episode.shape}, step shape: {step.shape}')
+                            print(f'Latent: {latent}, action: {action}, episode: {episode}, step: {step}')
                             latent_action_pairs.append(np.concatenate([latent.astype(np.float16), action.astype(np.float16), episode.astype(np.float16), step.astype(np.float16)]))
                     else:    
                         for latent, action in zip(latent_vectors, actions):
