52ee8e2e0e32e5a4c5755580f1fab8712ac81d4f
Dean <deanvanaswegen.dva@gmail.com>
2023-09-20 07:09:02 +0200

    train mdnrnn code

--- worldmodels1/src/worldmodels1/train_rnn.py
+++ worldmodels1/src/worldmodels1/train_rnn.py
@@ -3,9 +3,13 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.distributions.normal import Normal
-from utils import get_dataloader #TO DO THIS NEEDS TO BE A DIFFERENT DATALOADER FOR THE SEQUENCE DATA
+from utils import MemoryModelDataset
 from mdnrnn import MemoryModel  # Import MemoryModel from mdnrnn.py
+import numpy as np
+import logging
+from tqdm import tqdm
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Moved the argparse section to here so that the arguments can be parsed before the 'if __name__ == '__main__':'
 parser = argparse.ArgumentParser(description='Train Memory Model')
@@ -17,8 +21,9 @@ parser.add_argument('--n_gaussians', type=int, default=5, help='Number of Gaussi
 parser.add_argument('--data_path', type=str, help='Path to the training data')
 parser.add_argument('--save_path', type=str, default='./memory_model.pth', help='Where to save the model')
 parser.add_argument('--seq_len', type=int, default=50, help='Sequence length for LSTM input')  # Added seq_len argument
-args = parser.parse_args()
+parser.add_argument('--latent_dim', type=int, default=32, help='Latent dimension of VAE')  # Added latent_dim argument
 
+args = parser.parse_args()
 
 # Define loss function
 def loss_function(pi, mu, sigma, y):
@@ -30,32 +35,46 @@ def loss_function(pi, mu, sigma, y):
 
 # Main training loop
 if __name__ == '__main__':
+    #check if cuda is available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f'Using device: {device}')
+
+    # Load your saved data
+    latent_action_pairs = np.load(args.data_path, allow_pickle=True)
+    logging.info('Data loaded successfully')
+
     # Initialize the model and optimizer
-    model = MemoryModel(n_input=32, n_hidden=args.n_hidden, n_output=32, n_gaussians=args.n_gaussians)
+    model = MemoryModel(n_input=32, n_hidden=args.n_hidden, n_gaussians=args.n_gaussians)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    logging.info("Model and optimizer initialized")
 
     # Load your dataset
-    dataset = get_dataloader(args.data_path, seq_len=args.seq_len)  # Implement your DataLoader to handle sequence length
+    dataset = MemoryModelDataset(latent_action_pairs, seq_length=args.seq_len, latent_dim=args.latent_dim)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    logging.info('Dataset and dataloader initialized')
 
-    for epoch in range(args.epochs):
-        for i, batch in enumerate(dataloader):
+    best_loss = np.inf
+    for epoch in tqdm(range(args.epochs), desc='Epochs'):  # Wrap the epoch loop with tqdm
+        batch_tqdm = tqdm(dataloader, desc=f'Epoch {epoch + 1}', leave=False)  # Create a tqdm object for the dataloader loop
+        for i, batch in enumerate(batch_tqdm):  
             # Zero the gradients
             optimizer.zero_grad()
 
             # Forward pass
-            pi, mu, sigma = model(batch['input'])  # Your DataLoader should yield a dict with 'input' and 'target'
+            pi, mu, sigma = model(batch['input'].to(device))  # Ensure you use 'input' from the dict and send it to device
 
             # Compute loss
-            loss = loss_function(pi, mu, sigma, batch['target'])
+            loss = loss_function(pi, mu, sigma, batch['target'].to(device))  # Ensure you use 'target' from the dict and send it to device
 
             # Backward pass and optimization
             loss.backward()
             optimizer.step()
 
-            # Logging
-            if i % 10 == 0:  # Log every 10 batches
-                print(f'Epoch [{epoch + 1}/{args.epochs}], Batch [{i + 1}/{len(dataloader)}], Loss: {loss.item():.4f}')
+            # Update tqdm
+            if i % 10 == 0:  # Update every 10 batches
+                batch_tqdm.set_postfix({"Loss": f"{loss.item():.4f}"})
 
-    # Save the model
-    torch.save(model.state_dict(), args.save_path)
+    # Save the model if it has the best loss
+    if loss.item() < best_loss:
+        torch.save(model.state_dict(), args.save_path)
+        logging.info(f'Model saved to {args.save_path}, previous best loss: {best_loss:.4f}, current best loss: {loss.item():.4f}')
