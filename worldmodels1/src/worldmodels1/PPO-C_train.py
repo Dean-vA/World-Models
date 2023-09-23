f12b64e97b823a523ed68d375c2aaf216ccf68d4
Dean <deanvanaswegen.dva@gmail.com>
2023-09-23 12:43:57 +0200

    verbose

--- worldmodels1/src/worldmodels1/PPO-C_train.py
+++ worldmodels1/src/worldmodels1/PPO-C_train.py
@@ -2,15 +2,16 @@ from stable_baselines3 import PPO
 from gym_wrapper import CarRacingWrapper  
 import gymnasium as gym
 from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 
 def make_env():
-    original_env = gym.make('CarRacing-v2')
+    original_env = Monitor(gym.make('CarRacing-v2'))
     wrapped_env = CarRacingWrapper(original_env)
     return wrapped_env
 
 envs = DummyVecEnv([make_env for _ in range(64)])  # Four parallel environments
 
-model = PPO("MlpPolicy", envs, verbose=2)
+model = PPO("MlpPolicy", envs, verbose=1)
 
 # # Wrap the environment
 # wrapped_env = CarRacingWrapper(original_env)
