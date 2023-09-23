74c38e21b58af481297efdae6146dd6979b21743
Dean <deanvanaswegen.dva@gmail.com>
2023-09-23 11:01:44 +0200

    no progress bar

--- worldmodels1/src/worldmodels1/PPO-C_train.py
+++ worldmodels1/src/worldmodels1/PPO-C_train.py
@@ -20,7 +20,7 @@ model = PPO("MlpPolicy", envs, verbose=2)
 
 # Train the model in a loop saving checkpoints at every 20000 steps and continue training from the last saved checkpoint
 for i in range(100):
-    model.learn(total_timesteps=20000, reset_num_timesteps=False, progress_bar=True)
+    model.learn(total_timesteps=20000, reset_num_timesteps=False)
     model.save("ppo_car_racing_{}".format(i))
     print("Saved model checkpoint to {}".format("ppo_car_racing_{}".format(i)))
 
