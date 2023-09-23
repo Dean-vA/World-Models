0e35ea7a3c368a06e43d495aa98a24fb9241ae7d
Dean <deanvanaswegen.dva@gmail.com>
2023-09-23 10:15:55 +0200

    added checkpoints

--- worldmodels1/src/worldmodels1/PPO-C_train.py
+++ worldmodels1/src/worldmodels1/PPO-C_train.py
@@ -11,9 +11,8 @@ wrapped_env = CarRacingWrapper(original_env)
 # Initialize PPO model with a predefined policy (MlpPolicy)
 model = PPO("MlpPolicy", wrapped_env, verbose=2)
 
-# Train the model
-model.learn(total_timesteps=20000)
-
-model.save("ppo_car_racing")
-
+# Train the model in a loop saving checkpoints at every 20000 steps and continue training from the last saved checkpoint
+for i in range(100):
+    model.learn(total_timesteps=20000, reset_num_timesteps=False)
+    model.save("ppo_car_racing_{}".format(i))
 
