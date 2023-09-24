081421c62b435e8a3c91a3868671a88400b3df1f
Dean <deanvanaswegen.dva@gmail.com>
2023-09-24 08:15:46 +0200

    added model option to rollouts

--- worldmodels1/src/worldmodels1/rollouts.py
+++ worldmodels1/src/worldmodels1/rollouts.py
@@ -4,6 +4,9 @@ import argparse
 from multiprocessing import Pool, current_process
 import logging
 from PIL import Image
+import torch
+from stable_baselines3 import PPO
+
 
 
 def preprocess_state(state, img_size=64, gray_scale=False):
@@ -18,8 +21,43 @@ def preprocess_state(state, img_size=64, gray_scale=False):
     
     return state
 
+# a function to process the observation for the provided controller if any
+def process_obs(obs, action, vae, rnn, hidden, device='cpu'):
+    # obs is 96x96x3 need to convert to 64x64x1. 
+    # Convert to PyTorch tensor and normalize and permute dimensions and transfer to device
+    obs = torch.from_numpy(obs).permute(2, 0, 1).float().to(device) / 255.0  
+    # Rescale and average color channels
+    obs = torch.nn.functional.interpolate(obs.unsqueeze(0), size=(64, 64), mode='bilinear', align_corners=False)
+    obs = obs.mean(dim=1, keepdim=True)  # Reduce color channels by taking the mean
 
-def collect_data(env_name, num_episodes=10, max_steps=1000, seed=None, img_size=64, gray_scale=False):
+    with torch.no_grad():
+        #print(f'obs shape: {obs.shape}')
+        mu, logvar = vae.encode(obs)
+        z_t = vae.reparameterize(mu, logvar)
+        #print(f'z_t shape: {z_t.shape}')
+        # check if action is a tensor
+        if not torch.is_tensor(action):
+            action = torch.tensor(action).float().to(device).unsqueeze(0)
+        rnn_in = torch.cat((z_t, action), dim=1).unsqueeze(0)
+        #print(f"obs shape: {obs.shape}")
+        #print(f"action shape: {action.shape}")
+        #print(f"rnn_in shape: {rnn_in.shape}")
+        #print(f"Initial hidden states shapes: {self.hidden[0].shape}, {self.hidden[1].shape}")
+        _, _, _, hidden = rnn(rnn_in, hidden)
+        # extract hidden state
+        h_t = hidden[0]
+        #print(f"Final hidden states shapes: {self.hidden[0].shape}, {self.hidden[1].shape}")
+        #print(f"h_t shape: {h_t.shape}")
+        #print(f"z_t shape: {z_t.shape}")
+        # concat z_t and hidden
+        z_t = z_t.squeeze(0) # remove batch dimension
+        h_t = h_t.squeeze(0).squeeze(0) # remove batch and sequence dimensions
+        obs = torch.cat((z_t, h_t))
+        #print(f"obs shape: {obs.shape}")
+    return obs
+
+
+def collect_data(env_name, num_episodes=10, max_steps=1000, seed=None, img_size=64, gray_scale=False, worldmodel=None):
     worker_id = current_process()._identity[0]
     logging.info(f"Worker {worker_id}: Starting data collection for {num_episodes} episodes.")
     env = gym.make(env_name)
@@ -37,13 +75,28 @@ def collect_data(env_name, num_episodes=10, max_steps=1000, seed=None, img_size=
         done = False
         episode_data = []
         step_count = 0
-        
+
+        # Initialize the controller initial hidden state and action if provided
+        if worldmodel is not None:
+            hidden = (torch.zeros((1, 1, controller['rnn'].n_hidden)), 
+                      torch.zeros((1, 1, controller['rnn'].n_hidden)))
+            action = torch.zeros((1, 3))
+
         while not done and step_count < max_steps:
-            action = env.action_space.sample()
+            # Sample a random action from the environment's action space if no controller is provided
+            if worldmodel is None:
+                action = env.action_space.sample()
+            else:
+                obs = process_obs(state, action, worldmodel['vae'], worldmodel['rnn'], hidden)
+                action, _ = worldmodel['controller'].predict(obs)
+
             next_state, reward, done, truncated, info = env.step(action)
             episode_data.append((state, action, reward, done, episode, step_count)) #Step count and episode number to help with debugging
             # episode_data.append((state, action, reward, next_state, done, truncated, info))
-            state = preprocess_state(next_state, img_size=img_size, gray_scale=gray_scale)
+            if worldmodel is None:
+                state = preprocess_state(next_state, img_size=img_size, gray_scale=gray_scale) 
+            else:
+                state = next_state
             step_count += 1
             
         data.append(episode_data)
@@ -65,15 +118,49 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, help="Base random seed")
     parser.add_argument("--img_size", default=64, type=int, help="Image size")
     parser.add_argument("--gray_scale", action="store_true", default=False, help="Convert images to grayscale")
+    parser.add_argument("--output_path", default="collected_data.npy", type=str, help="Path to save the collected data")
+    parser.add_argument("--use_controller", action="store_true", default=False, help="Use trained controller for rollouts")
+    parser.add_argument("--controller_path", default="", type=str, help="Path to the trained controller model")
     args = parser.parse_args()
+
+    if args.use_controller:
+        latent_dim = 32
+        action_dim = 3
+        from cnnvae import VAE
+        from mdnrnn import MemoryModel
+        if args.controller_path == "":
+            print("Controller path must be specified when using a trained controller.")
+            exit(1)
+        
+        vae = VAE()
+        # Load the state_dict into CPU memory
+        state_dict = torch.load('./../vae2.pth', map_location='cpu')
+        # Remove 'module.' prefix from state_dict keys
+        new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+        # Load the modified state_dict into the model
+        vae.load_state_dict(new_state_dict)
+        vae.eval()
+        
+        rnn = MemoryModel(n_input=latent_dim+action_dim, n_hidden=256, n_gaussians=5, latent_dim=latent_dim)
+        # Load the state_dict into CPU memory
+        state_dict = torch.load('./../src/worldmodels1/memory_model.pth', map_location='cpu')
+        # Remove 'module.' prefix from state_dict keys
+        new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+        # Load the modified state_dict into the model
+        rnn.load_state_dict(new_state_dict)
+        rnn.eval()
+        
+        controller = PPO.load(args.controller_path)  # Use the specified path
+        
+        worldmodel = {'vae': vae, 'rnn': rnn, 'model': controller}
     
     logging.info(f"Starting data collection for {args.episodes * args.workers} episodes.")
     with Pool(args.workers) as p:
         results = p.starmap(
             collect_data, 
-            [(args.env, args.episodes, args.max_steps, args.seed, args.img_size, args.gray_scale) for _ in range(args.workers)]
+            [(args.env, args.episodes, args.max_steps, args.seed, args.img_size, args.gray_scale, worldmodel) for _ in range(args.workers)]
         )
 
     collected_data = [item for sublist in results for item in sublist]
-    np.save("collected_data.npy", np.array(collected_data, dtype=object))
+    np.save(args.output_path, np.array(collected_data, dtype=object))
 
