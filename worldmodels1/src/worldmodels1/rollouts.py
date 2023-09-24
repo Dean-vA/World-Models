7ddf2c7e7f805fbb7a5501de6c9fe839045483b3
Dean <deanvanaswegen.dva@gmail.com>
2023-09-24 09:09:20 +0200

    .

--- worldmodels1/src/worldmodels1/rollouts.py
+++ worldmodels1/src/worldmodels1/rollouts.py
@@ -86,6 +86,7 @@ def collect_data(env_name, num_episodes=10, max_steps=1000, seed=None, img_size=
 
         while not done and step_count < max_steps:
             logging.info(f"Worker {worker_id}: Starting step {step_count + 1}/{max_steps}.")
+            print(f"Worker {worker_id}: Starting step {step_count + 1}/{max_steps}.")
             # Sample a random action from the environment's action space if no controller is provided
             if worldmodel is None:
                 action = env.action_space.sample()
