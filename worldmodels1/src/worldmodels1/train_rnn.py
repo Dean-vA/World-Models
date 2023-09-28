b495b4e999e84881877ba5ea5b1e3e24f2689d1b
Dean <deanvanaswegen.dva@gmail.com>
2023-09-28 08:09:12 +0200

    .

--- worldmodels1/src/worldmodels1/train_rnn.py
+++ worldmodels1/src/worldmodels1/train_rnn.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
             optimizer.zero_grad()
 
             # Forward pass
-            pi, mu, sigma, hidden = model(batch[0].to(device))  # [0] is the input sequence from the dataset #TO DO: check if this is correct should hidden be passed in during forward pass in training?
+            pi, mu, sigma, hidden = model(batch[0].to(device), hidden)  # [0] is the input sequence from the dataset #TO DO: check if this is correct should hidden be passed in during forward pass in training?
             if i == 0:
                 logging.info(f'pi shape: {pi.shape}, mu shape: {mu.shape}, sigma shape: {sigma.shape}, y shape: {batch[1].shape}')
             
